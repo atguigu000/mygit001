322b788c545dc44c29a7d4bbe9ce02b8d36b2ecf
atguigu000 <1551446540@qq.com>
2020-09-07 18:34:56 +0800

    commit --- github

--- src/main/java/com/atguigu/git/TestDemon.java
+++ src/main/java/com/atguigu/git/TestDemon.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class TestDemon {
     public static void main(String[] args) {
         System.out.println("我是分支创建的!");
+        System.out.println("我是测试分支的！");
     }
 }
